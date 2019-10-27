7bf762ddb5c7ce161a95c98647637b60bf8db791
Zhangsc-js <874649060@qq.com>
2019-10-27 17:05:11 +0800

    right完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,13 +1,51 @@
 <template>
-  <h1>rights</h1>
+  <div class="right">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-table :data="rightList" style="width:100%">
+      <el-table-column type="index"></el-table-column>
+      <el-table-column prop="authName" label="权限名称"></el-table-column>
+      <el-table-column prop="path" label="路径"></el-table-column>
+      <el-table-column prop="level" label="层级">
+        <template v-slot:default="{row}">
+          <span v-if="row.level==='0'">一级</span>
+          <span v-if="row.level==='1'">二级</span>
+          <span v-if="row.level==='2'">三级</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      rightList: []
+    }
+  },
+  created () {
+    this.getRightList()
+  },
+  methods: {
+    async getRightList () {
+      const { meta, data } = await this.$axios.get(`rights/list`)
+      if (meta.status === 200) {
+        console.log(data)
 
+        this.rightList = data
+      } else {
+        this.$message.error(meta.msg)
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
